7cc994ec97e3aa6164b7e3c5a1b260ba419ceb7b
MichaelXcX <ciuciuletemihaicm@gmail.com>
2025-03-30 17:47:16 +0300

    detailed email

--- web/backend/routes/notifiers.route.js
+++ web/backend/routes/notifiers.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const twilio = require("twilio"); // Or, for ESM: import twilio from "twilio";
 const mailer = require('../helpers/mailer.js')
 const router = express.Router()
-
+const { Elder } = require('../models/elder.model.js')
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
@@ -15,17 +15,34 @@ router.post('/warn', async (req, res) => {
     //     to: "+40737924300",
     //   });
     //   console.log(message.body);
-    const { phone } = req.body;
+    const { phone, location } = req.body;
+    // Date format YYYY-MM-DD hh:mm:ss
+    console.log(phone, location)
+    const today = new Date().toISOString().slice(0, 19).replace("T", " ");
+    const elder = await Elder.findOne({ phone });
     const content = {
         subject: "Elder fall warning",
-        text: `One of your elders has fallen. Please check on them.`,
+        text: `One of your elders, ${elder.name}, has fallen. Please check on them. Their location is ${location}. Time when the fall was detected: ${today}.`,
     };
     try {
         await mailer.sendEmail("ciuciuletemihaicm@gmail.com", content);
         console.log("Email sent successfully");
-    } catch (error) {
+    } 
+    catch (error) {
         console.error("Error sending email:", error);
     }
+
+    // try {
+    //      const message = await client.messages.create({
+    //         body: "One of your elders has fallen. Please check on them.",
+    //         from: "+15642346384",
+    //         to: "+40737924300",
+    //     });
+    //     console.log(message.body);
+    // }
+    // catch (error) {
+    //     console.error("Error sending SMS:", error);
+    // }
 })
 
 module.exports = router
